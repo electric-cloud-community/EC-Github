8416e68fc257f65ec2c46c0dedb0c7896ba18ca7
Polina Shubina <925043@gmail.com>
2019-03-05 13:25:25 +0200

    Adding license to the repository

--- dsl/procedures/CreateRepository/steps/createRepository.groovy
+++ dsl/procedures/CreateRepository/steps/createRepository.groovy
@@ -10,16 +10,14 @@ def config = new EFPlugin().getConfiguration(parameters.config)
 GithubPlugin plugin = new GithubPlugin(config.userName, config.password)
 String owner = parameters.owner
 String repo = parameters.repo
-GHRepository repository = plugin.createRepository(owner, repo, [
-    description: parameters.description,
-    private: parameters.public == 'false',
-    teams: parameters.teams?.split('\n+')?.collect {
-        def (name, permission) = it.split(/\s*:\s*/)
-        [name: name, permission: permission]
-    },
-    branchProtectionRules: parameters.branchProtectionRules?.split(/\n+/)?.collect {
-        def (branchName, rules) = it.split(/\s*:\s*/)
+
+def branchProtectionRules = parameters.branchProtectionRules?.split(/\n+/)?.collect {
+    def branchNameRules = it.split(/\s*:\s*/)
+    if (branchNameRules?.size() == 2) {
+        def rules = branchNameRules[1]
+        def branchName = branchNameRules[0]
         def rulesList = rules.split(/\s*,\s*/)
+
         def retval = [branchName: branchName]
         rulesList.each {
             def (key, value) = it.split(/\s*=\s*/)
@@ -27,6 +25,23 @@ GHRepository repository = plugin.createRepository(owner, repo, [
         }
         retval
     }
+}?.findAll { it }
+
+
+def teams = parameters.teams?.split('\n+')?.collect {
+    def namePermission = it.split(/\s*:\s*/)
+    if (namePermission?.size() == 2) {
+        [name: namePermission[0], permission: namePermission[1]]
+    }
+}?.findAll { it }
+
+GHRepository repository = plugin.createRepository(owner, repo, [
+    description          : parameters.description,
+    private              : parameters.public == 'false',
+    teams                : teams,
+    branchProtectionRules: branchProtectionRules,
+    addLicense           : parameters.addLicense == "true",
+    licenseFile          : parameters.licenseFile,
 ])
 
 println "Repository: ${repository.htmlUrl}"
--- dsl/properties/scripts/GithubPlugin.groovy
+++ dsl/properties/scripts/GithubPlugin.groovy
@@ -3,11 +3,15 @@ import groovy.util.logging.Slf4j
 import org.kohsuke.github.GHBranch
 import org.kohsuke.github.GHBranchProtection
 import org.kohsuke.github.GHBranchProtectionBuilder
+import org.kohsuke.github.GHContent
+import org.kohsuke.github.GHContentUpdateResponse
 import org.kohsuke.github.GHCreateRepositoryBuilder
+import org.kohsuke.github.GHIssueState
 import org.kohsuke.github.GHOrganization
 import org.kohsuke.github.GHPermissionType
 import org.kohsuke.github.GHPerson
 import org.kohsuke.github.GHPullRequest
+import org.kohsuke.github.GHPullRequestReviewEvent
 import org.kohsuke.github.GHRef
 import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GHTeam
@@ -21,6 +25,8 @@ import org.kohsuke.github.GitHub
 class GithubPlugin {
     GitHub client
 
+    final String LICENSE = 'LICENSE'
+
     def GithubPlugin(String username, String password) {
         client = GitHub.connect(username, password)
     }
@@ -29,18 +35,57 @@ class GithubPlugin {
 
     }
 
+    GHPullRequest createPullRequest(String ownerName, String repoName, String sourceBranch, String destBranch, Map parameters) {
+        GHRepository repository
+        try {
+            repository = client.getRepository("${ownerName}/${repoName}")
+        } catch (IOException e) {
+            log.info "Repository ${ownerName}/${repoName} does not exist"
+            throw e
+        }
+        String title = parameters.title
+        if (!title) {
+            String lastCommitSha = repository.getBranch(sourceBranch).SHA1
+            String lastCommitMessage = repository.getCommit(lastCommitSha).commitShortInfo.message
+            title = lastCommitMessage
+        }
+        GHPullRequest request = repository.createPullRequest(title, sourceBranch, destBranch, "")
+        log.info "Created Pull Request: ${request.htmlUrl}"
+        log.info "Mergeable: ${request.mergeable}"
+
+        if (parameters.approve) {
+            request.createReview().event(GHPullRequestReviewEvent.APPROVE).create()
+            log.info "Approved pull request #${request.number}"
+        }
+
+        if (parameters.merge) {
+            if (request.mergeable) {
+                log.info "Trying to auto-merge request #${request.number}"
+                request.merge()
+                log.info "Merged #${request.number} automatically"
+            } else {
+                log.info "Request #${request.number} cannot be merged automatically"
+            }
+        }
+        return request
+    }
+
     GHRepository createRepository(String ownerName, String repoName, Map parameters, String updateAction = 'update') {
         GHRepository repository
         String description = parameters.description
         boolean isPrivate = parameters.private
+        if (!repoName) {
+            throw new RuntimeException("Repository name should be provided")
+        }
         try {
             repository = client.getRepository("${ownerName}/${repoName}")
             log.info "Repository ${repository.name} already exists"
-        } catch (IOException e) {
+        } catch (Throwable e) {
             GHCreateRepositoryBuilder repositoryBuilder
             try {
                 repositoryBuilder = client.getOrganization(ownerName).createRepository(repoName)
-            } catch (IOException e2) {
+            } catch (Throwable e2) {
+                log.info "Owner is a user"
                 repositoryBuilder = client.createRepository(repoName)
             }
             if (description) {
@@ -52,6 +97,7 @@ class GithubPlugin {
         }
 
         if (description && repository.description != description) {
+            log.info "Setting description to $description"
             repository.setDescription(description)
         }
 
@@ -60,7 +106,7 @@ class GithubPlugin {
         }
 
 
-        if (parameters.teams) {
+        if (parameters.teams?.size() > 0) {
             GHOrganization org = client.getOrganization(ownerName)
             def teams = [:]
             parameters.teams.each {
@@ -97,8 +143,42 @@ class GithubPlugin {
             log.info "Enabled protection for branch ${branchName}"
         }
 
+        if (parameters.addLicense) {
+            File licenseFile = new File(parameters.licenseFile as String)
+            if (!licenseFile.exists()) {
+                throw new RuntimeException("License file ${licenseFile.absolutePath}")
+            }
+
+            try {
+                GHContent content = repository.getFileContent(LICENSE)
+                if (content.content == licenseFile.text) {
+                    log.info "No update is required"
+                } else {
+                    String sha = content.sha
+                    GHContentUpdateResponse response = repository.createContent()
+                                                                 .path(LICENSE)
+                                                                 .message("Updated LICENSE")
+                                                                 .sha(sha)
+                                                                 .content(licenseFile.text)
+                                                                 .commit()
+                    log.info "Updated license: ${response.commit.SHA1}"
+                    log.info response.commit.htmlUrl as String
+                }
+            } catch (IOException e) {
+                log.info "License file does not exist in the repository"
+                GHContentUpdateResponse response = repository.createContent()
+                                                             .path(LICENSE)
+                                                             .content(licenseFile.text)
+                                                             .message("Created LICENSE")
+                                                             .commit()
+                log.info "Created License: ${response.commit.SHA1}"
+                log.info response.commit.htmlUrl as String
+            }
+        }
+
         return repository
     }
+
 
     @Memoized
     GHBranch ensureBranch(GHRepository repository, String branchName) {
